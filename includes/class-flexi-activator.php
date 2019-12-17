48ba08418242dbfc6d4b695b37f8bfc8c74b4251
ODude Navneet Gupta <navneet@odude.com>
2019-12-17 18:57:59 +0545

    initial

---
+++ includes/class-flexi-activator.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://odude.com/
+ * @since      1.0.0
+ *
+ * @package    Flexi
+ * @subpackage Flexi/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Flexi
+ * @subpackage Flexi/includes
+ * @author     ODude <navneet@odude.com>
+ */
+class Flexi_Activator
+{
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate()
+	{
+
+		/**
+		 * Custom Post Types
+		 */
+		require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-flexi-post_types.php';
+		$plugin_post_types = new Flexi_Post_Types();
+		$plugin_post_types->create_custom_post_type();
+		flush_rewrite_rules();
+	}
+}
---
+++ includes/class-flexi.php
@@ -0,0 +1,229 @@
+<?php
+
+/**
+ * The file that defines the core plugin class
+ *
+ * A class definition that includes attributes and functions used across both the
+ * public-facing side of the site and the admin area.
+ *
+ * @link       https://odude.com/
+ * @since      1.0.0
+ *
+ * @package    Flexi
+ * @subpackage Flexi/includes
+ */
+
+/**
+ * The core plugin class.
+ *
+ * This is used to define internationalization, admin-specific hooks, and
+ * public-facing site hooks.
+ *
+ * Also maintains the unique identifier of this plugin as well as the current
+ * version of the plugin.
+ *
+ * @since      1.0.0
+ * @package    Flexi
+ * @subpackage Flexi/includes
+ * @author     ODude <navneet@odude.com>
+ */
+class Flexi
+{
+
+	/**
+	 * The loader that's responsible for maintaining and registering all hooks that power
+	 * the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      Flexi_Loader    $loader    Maintains and registers all hooks for the plugin.
+	 */
+	protected $loader;
+
+	/**
+	 * The unique identifier of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+	 */
+	protected $plugin_name;
+
+	/**
+	 * The current version of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $version    The current version of the plugin.
+	 */
+	protected $version;
+
+	/**
+	 * Define the core functionality of the plugin.
+	 *
+	 * Set the plugin name and the plugin version that can be used throughout the plugin.
+	 * Load the dependencies, define the locale, and set the hooks for the admin area and
+	 * the public-facing side of the site.
+	 *
+	 * @since    1.0.0
+	 */
+	public function __construct()
+	{
+		if (defined('FLEXI_VERSION')) {
+			$this->version = FLEXI_VERSION;
+		} else {
+			$this->version = '1.0.0';
+		}
+		$this->plugin_name = 'flexi';
+
+		$this->load_dependencies();
+		$this->set_locale();
+		$this->define_admin_hooks();
+		$this->define_public_hooks();
+	}
+
+	/**
+	 * Load the required dependencies for this plugin.
+	 *
+	 * Include the following files that make up the plugin:
+	 *
+	 * - Flexi_Loader. Orchestrates the hooks of the plugin.
+	 * - Flexi_i18n. Defines internationalization functionality.
+	 * - Flexi_Admin. Defines all hooks for the admin area.
+	 * - Flexi_Public. Defines all hooks for the public side of the site.
+	 *
+	 * Create an instance of the loader which will be used to register the hooks
+	 * with WordPress.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function load_dependencies()
+	{
+
+		/**
+		 * Custom Post Types (flexi & flexi_category)
+		 */
+		require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-flexi-post_types.php';
+
+		/**
+		 * The class responsible for orchestrating the actions and filters of the
+		 * core plugin.
+		 */
+		require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-flexi-loader.php';
+
+		/**
+		 * The class responsible for defining internationalization functionality
+		 * of the plugin.
+		 */
+		require_once plugin_dir_path(dirname(__FILE__)) . 'includes/class-flexi-i18n.php';
+
+		/**
+		 * The class responsible for defining all actions that occur in the admin area.
+		 */
+		require_once plugin_dir_path(dirname(__FILE__)) . 'admin/class-flexi-admin.php';
+
+		/**
+		 * The class responsible for defining all actions that occur in the public-facing
+		 * side of the site.
+		 */
+		require_once plugin_dir_path(dirname(__FILE__)) . 'public/class-flexi-public.php';
+
+		$this->loader = new Flexi_Loader();
+	}
+
+	/**
+	 * Define the locale for this plugin for internationalization.
+	 *
+	 * Uses the Flexi_i18n class in order to set the domain and to register the hook
+	 * with WordPress.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function set_locale()
+	{
+
+		$plugin_i18n = new Flexi_i18n();
+
+		$this->loader->add_action('plugins_loaded', $plugin_i18n, 'load_plugin_textdomain');
+	}
+
+	/**
+	 * Register all of the hooks related to the admin area functionality
+	 * of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function define_admin_hooks()
+	{
+		//custom post type
+		$plugin_post_types = new Flexi_Post_Types();
+		$this->loader->add_action('init', $plugin_post_types, 'create_custom_post_type', 999);
+		$plugin_admin = new Flexi_Admin($this->get_plugin_name(), $this->get_version());
+
+		$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_styles');
+		$this->loader->add_action('admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts');
+	}
+
+	/**
+	 * Register all of the hooks related to the public-facing functionality
+	 * of the plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   private
+	 */
+	private function define_public_hooks()
+	{
+
+		$plugin_public = new Flexi_Public($this->get_plugin_name(), $this->get_version());
+
+		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_styles');
+		$this->loader->add_action('wp_enqueue_scripts', $plugin_public, 'enqueue_scripts');
+	}
+
+	/**
+	 * Run the loader to execute all of the hooks with WordPress.
+	 *
+	 * @since    1.0.0
+	 */
+	public function run()
+	{
+		$this->loader->run();
+	}
+
+	/**
+	 * The name of the plugin used to uniquely identify it within the context of
+	 * WordPress and to define internationalization functionality.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The name of the plugin.
+	 */
+	public function get_plugin_name()
+	{
+		return $this->plugin_name;
+	}
+
+	/**
+	 * The reference to the class that orchestrates the hooks with the plugin.
+	 *
+	 * @since     1.0.0
+	 * @return    Flexi_Loader    Orchestrates the hooks of the plugin.
+	 */
+	public function get_loader()
+	{
+		return $this->loader;
+	}
+
+	/**
+	 * Retrieve the version number of the plugin.
+	 *
+	 * @since     1.0.0
+	 * @return    string    The version number of the plugin.
+	 */
+	public function get_version()
+	{
+		return $this->version;
+	}
+}
