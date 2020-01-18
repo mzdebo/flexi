6a6530dd79cfe426a0d1314ada6078f143a8a749
ODude Navneet Gupta <navneet@odude.com>
2020-01-18 18:36:48 +0545

    style

--- public/partials/layout/gallery/regular/loop.php
+++ public/partials/layout/gallery/regular/loop.php
@@ -1,9 +1,12 @@
 <?php
 $data = flexi_image_data('flexi-thumb', $post);
 ?>
-<div class="pure-u-1 pure-u-md-1-4">
+<div class="pure-u-1 pure-u-md-1-2 flexi_gallery_child upg_image-frame flexi_border" id="flexi_<?php echo get_the_ID(); ?>" data-tags="<?php echo $tags; ?>">
 
-    <img class="flexi-fit_cover" src="<?php echo esc_url(flexi_image_src('flexi-medium', $post)); ?>">
-
+    <a <?php echo $data['extra'] . ' href="' . $data['url'] . '" data-caption="' . $data['title'] . '" border="0"'; ?>>
+    <img class="flexi-fit_cover upg_hover-zoom" src="<?php echo esc_url(flexi_image_src('flexi-medium', $post)); ?>">
+    <?php echo ' <flexi_figcaption><b>' . $data['title'] . '</b><br>' . get_the_excerpt() . '</flexi_figcaption>'; ?>
+</a>
+</div>
 </div>
 
