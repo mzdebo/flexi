d4b5069b57f130cd5c10ebbae04758c7b89d21de
ODude Navneet Gupta <navneet@odude.com>
2020-02-01 16:11:23 +0545

    h3 into div

--- public/partials/layout/gallery/regular/loop.php
+++ public/partials/layout/gallery/regular/loop.php
@@ -4,20 +4,19 @@ $data = flexi_image_data('flexi-thumb', $post);
 
 <div class="pure-u-1 pure-u-md-1-<?php echo $column; ?> flexi_gallery_child flexi_padding" id="flexi_<?php echo get_the_ID(); ?>" style="position: relative;" data-tags="<?php echo $tags; ?>">
 
-<div class="flexi_effect" id="<?php echo $hover_effect; ?>">
+      <div class="flexi_effect" id="<?php echo $hover_effect; ?>">
 
 
-      <a <?php echo $data['extra'] . ' href="' . $data['url'] . '" data-caption="' . $data['title'] . '" border="0"'; ?>>
-     <img class="flexi-fit_cover flexi_image_frame"  src="<?php echo esc_url(flexi_image_src('flexi-medium', $post)); ?>">
-      <?php echo ' <flexi_figcaption><b>' . $data['title'] . '</b><br>' . get_the_excerpt() . '</flexi_figcaption>'; ?>
+            <a <?php echo $data['extra'] . ' href="' . $data['url'] . '" data-caption="' . $data['title'] . '" border="0"'; ?>>
+                  <img class="flexi-fit_cover flexi_image_frame"  src="<?php echo esc_url(flexi_image_src('flexi-medium', $post)); ?>">
+                  <?php echo ' <flexi_figcaption><b>' . $data['title'] . '</b><br>' . flexi_excerpt() . '</flexi_figcaption>'; ?>
 
-
-      <div id="flexi_title" class="<?php echo $hover_caption; ?>">
-      <h3><?php echo $data['title']; ?></h3>
-      <p><?php echo get_the_excerpt(); ?></p>
+                  <div id="flexi_info" class="<?php echo $hover_caption; ?>">
+                        <div class="flexi_title"><?php echo $data['title']; ?></div>
+                        <div class="flexi_p"><?php echo flexi_excerpt(); ?></div>
+                  </div>
+            </a>
       </div>
-      </a>
-</div>
 
 </div>
 
