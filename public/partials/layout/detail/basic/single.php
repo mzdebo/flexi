702af126720d6918ab6756a045aa0029aa6c85a7
ODude Navneet Gupta <navneet@odude.com>
2020-02-02 13:21:34 +0545

    content

--- public/partials/layout/detail/basic/single.php
+++ public/partials/layout/detail/basic/single.php
@@ -1,7 +1,25 @@
 <?php
-echo "<img src='" . flexi_image_src('flexi-large') . "' >";
-echo "<br>am liking it";
+/*
+<a href='<?php echo flexi_get_button_url(get_the_ID(), false, 'edit_flexi_page', 'flexi_form_settings'); ?>'>
+<?php echo __('Edit Post', 'flexi'); ?>
+</a>
+ */
 ?>
- <a href='<?php echo flexi_get_button_url(get_the_ID(), false, 'edit_flexi_page', 'flexi_form_settings'); ?>'>
-                <?php echo __('Edit Post', 'flexi'); ?>
-            </a> |
+<div class="pure-g">
+<div class="pure-u-1-1">
+
+<div class="flexi_margin-box">
+<?php
+echo "<div class='flexi_frame_1' style='text-align: center;'><img src='" . flexi_image_src('flexi-large') . "' ></div>";
+?>
+</div>
+<div class="pure-u-1">
+		<div class="flexi_margin-box">
+
+
+			<div class="flex-desp"> <?php echo $post->post_content; ?></div>
+
+		</div>
+	</div>
+</div>
+</div>
